fcbb7a9ba727a63f7d686a6bb7119bb36e442656
Teddy Milford <teddy.milford@gmail.com>
2022-06-27 20:05:39 -0600

    commit tree 971f5512aae3383f10494964f55ffd54689633a3

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
+class CashRegister
+  def initialize(price, discount = 1)
+    @total = 0
+    @price = price
+    @discount = discount
+  end
 
+  def total
+    (price * discount) + total = total
+  end
+end
